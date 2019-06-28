1f0c8b426a0500434d7425a3cfabce9030bf1c3c
Richard Hsu <rxh@google.com>
2019-06-28 15:41:54 -0400

    folder name

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "google_folder" "shared_services_folder" {
 resource "google_project" "shared_services_tf_project" {
   name = "Shared Services Terraform"
   project_id = "reechar-key-shared-services-tf"
-  folder_id = "${google_folder.shared_services_folder.folder_id}"
+  folder_id = "${google_folder.shared_services_folder.name}"
   billing_account = "${var.billing_account}"
 }
