1c789ea4cf7010e5439ca358cb3a0241312b0adb
Richard Hsu <rxh@google.com>
2019-06-28 15:38:44 -0400

    make a first project

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-resource "google_folder" "main" {
+resource "google_folder" "shared_services_folder" {
   display_name = "Shared services"
   parent       = "organizations/${var.org_id}"
 }
 
+resource "google_project" "shared_services_tf_project" {
+  name = "Shared Services Terraform"
+  folder_id = "${google_folder.shared_services_folder.folder_id}"
+  billing_account = "${var.billing_account}"
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "org_id" {
-  description = "Name of the GCP Folder to create"
+  description = "Organization ID of the gcp org"
   default = "177452797885"
+}
+
+variable "billing_account" {
+  description = "Default billing account to use for new projects"
+  default = "0030A3-D6BEAE-9C7088	"
 }
