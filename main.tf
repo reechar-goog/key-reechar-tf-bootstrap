491ea9f50d6c899476c509c69705f9005dbd5a69
Richard Hsu <rxh@google.com>
2019-06-28 16:16:45 -0400

    making repo and bucket

--- main.tf
+++ main.tf
@@ -20,3 +20,20 @@ resource "google_project_service" "shared_services_cb" {
   service = "cloudbuild.googleapis.com"
 }
 
+resource "google_project_service" "shared_services_csr" {
+  project = "${google_project.shared_services_tf_project.project_id}"
+  service = "sourcerepo.googleapis.com"
+}
+
+resource "google_sourcerepo_repository" "reechar_key_shared_services_tf_repo" {
+  project = "${google_project.shared_services_tf_project.project_id}"
+  name = "reechar-key-shared-services-tf"
+  depends_on = ["google_project_service.shared_services_csr"]
+}
+
+
+resource "google_storage_bucket" "shared_services_tf_state_bucket" {
+  project = "${google_project.shared_services_tf_project.project_id}"
+  name = "reechar-key-shared-services-tf"
+}
+
